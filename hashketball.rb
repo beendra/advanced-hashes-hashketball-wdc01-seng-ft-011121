1b753ae2d58a684cf097b95c778b9ea4a728a9e2
Alexandra R <alexandra.ricardel@gmail.com>
2020-11-28 04:47:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,15 +127,14 @@ def game_hash
 end
 
 
-def num_points_scored(name) 
-  hash = game_hash
-  game_hash.each do |teams_playing, data| 
-    data[:players].each do |player_name, points|
-      if  player_name == name
-        return hash[teams_playing][:players][name][:points]
-      end 
-    end 
-  end 
+def num_points_scored(player_search)
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:player_name] == player_search
+        return player[:points]
+      end
+    end
+  end
 end
 
 def shoe_size
